3df68b8139618bdf98f36ac66cc884395be19d88
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-08 11:43:02 +0300

    Added controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
+let index = require("../controllers/index.js");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Hello" });
-});
+router.get("/", index.index);
 
 module.exports = router;
